2a1cb9ffe47d14ca11d4742843f0e1d055608330
pekarsky.maxim@gmail.com <flufci@gmail.com>
2015-09-07 22:32:28 -0400

    Got error messages working, but no customization

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ActiveRecord::Base
   has_many :records
   has_secure_password
 
-   validates :email, uniqueness: true
-   validates :username, uniqueness: true
 
+   validates :email, presence: true, uniqueness: { message: "is already in use" }
+   validates :username, presence: true, uniqueness: { message: " is already in use" }
+   validates :age, presence: true
+   validates :zipcode, presence: true
+   validates :name, presence: true
 end

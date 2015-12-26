80d49d38ec243f5c440f6c3ed6135b2837a17343
pekarsky.maxim@gmail.com <flufci@gmail.com>
2015-12-26 00:28:47 -0500

    more heroku image tinkering

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,14 +3,14 @@ module ApplicationHelper
 
   def randomized_background_image
     images = [
-      '/search-page/1.jpg', 
-      '/search-page/2.jpg', 
-      '/search-page/3.jpg', 
-      '/search-page/4.jpg', 
-      '/search-page/5.jpg', 
-      '/search-page/6.jpg', 
-      '/search-page/7.jpg',
-      '/search-page/8.jpg',
+      '/assets/search-page/1.jpg', 
+      '/assets/search-page/2.jpg', 
+      '/assets/search-page/3.jpg', 
+      '/assets/search-page/4.jpg', 
+      '/assets/search-page/5.jpg', 
+      '/assets/search-page/6.jpg', 
+      '/assets/search-page/7.jpg',
+      '/assets/search-page/8.jpg',
       '/assets/search-page/9.jpg',
       '/assets/search-page/10.jpg',
       '/assets/search-page/11.jpg',

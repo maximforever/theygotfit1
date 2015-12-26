77e1c66ee8e4d085b9fccbab90a4210eda811be2
pekarsky.maxim@gmail.com <flufci@gmail.com>
2015-12-25 23:38:50 -0500

    switch to Zoho for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,8 +87,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false  
   config.action_mailer.default :charset => "utf-8"  
   config.action_mailer.smtp_settings = {  
-    address: "smtp.gmail.com",
-    port: 587,
+    address: "smtp.zoho.com",
+    port: 465,
     domain: "salty-journey-3992.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
